edfcc91b8a2a0f9c117e1d4e65e27404c9054e12
remciszewski <remciszewski@icloud.com>
2024-05-06 21:38:23 +0200

    add homepage animations

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,21 @@
 import './index.scss';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = [' R', 'e', 'm', 'i', 'g', 'i', 'u', 's', 'z']
+
     return (
         <div className='container home-page'>
             <div className='text-zone'>
-                <h1>Dzień dobry, <br /> Jestem Remek</h1>
+                <h1>Dzien dobry, <br />
+                <span>Jestem </span>
+                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={20} />
+                </h1>
                 <h2>Głaskam sra jak psa 🥸💩</h2>
-                <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+                <Link to='/contact' className='flat-button'>skontaktuj sie ze mnom</Link>
             </div>
         </div>
     );
