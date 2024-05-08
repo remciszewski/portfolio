058663104f74f2f2eec8cd25bb05ff5ae9462132
remciszewski <remciszewski@icloud.com>
2024-05-08 23:24:29 +0200

    add about styles and content

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,19 @@
 import './index.scss'
 
-const About = () => 'Hello'
+const About = () => {
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    About me
+                </h1>
+                <p>Dupa dupa</p>
+                <p>Kupa kupa</p>
+                <p>Cipa cipa</p>
+            </div>
+
+        </div>
+    )
+}
 
 export default About
