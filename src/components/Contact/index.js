d8012811b9eb79f719f9c84437924c1004527ef8
remciszewski <remciszewski@icloud.com>
2024-05-11 23:08:01 +0200

    add contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,6 +8,28 @@ const Contact = () => {
                     <h1>
                         Contact me
                     </h1>
+                    <p>
+                        acdefgh
+                    </p>
+                    <div className='contact-form'>
+                        <form>
+                        <ul>
+                            <li className='half'>
+                                <input type='text' name='name' placeholder='Name' required />
+                            </li>
+                            <li className='half'>
+                                <input type='email' name='email' placeholder='Email' required />
+                            </li>
+                            <li>
+                                <input placeholder='Subject' type='text' name='subject' required />
+                            </li>
+                            <li>
+                                <textarea placeholder='Message' name='message' required></textarea>
+                            </li>
+                            <submit type='submit' className='flat-button' value='SEND' />
+                        </ul>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
