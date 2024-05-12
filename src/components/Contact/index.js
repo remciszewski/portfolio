e6fe4d6a80119c1788ccc4078e453bd3a4136c2c
remciszewski <remciszewski@icloud.com>
2024-05-12 23:45:14 +0200

    add form styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -13,21 +13,23 @@ const Contact = () => {
                     </p>
                     <div className='contact-form'>
                         <form>
-                        <ul>
-                            <li className='half'>
-                                <input type='text' name='name' placeholder='Name' required />
-                            </li>
-                            <li className='half'>
-                                <input type='email' name='email' placeholder='Email' required />
-                            </li>
-                            <li>
-                                <input placeholder='Subject' type='text' name='subject' required />
-                            </li>
-                            <li>
-                                <textarea placeholder='Message' name='message' required></textarea>
-                            </li>
-                            <submit type='submit' className='flat-button' value='SEND' />
-                        </ul>
+                            <ul>
+                                <li className='half'>
+                                    <input type='text' name='name' placeholder='Name' required />
+                                </li>
+                                <li className='half'>
+                                    <input type='email' name='email' placeholder='Email' required />
+                                </li>
+                                <li>
+                                    <input placeholder='Subject' type='text' name='subject' required />
+                                </li>
+                                <li>
+                                    <textarea placeholder='Message' name='message' required></textarea>
+                                </li>
+                                <li>
+                                    <input type='submit' className='flat-button' value='SEND' />
+                                </li>
+                            </ul>
                         </form>
                     </div>
                 </div>
