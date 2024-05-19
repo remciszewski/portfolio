777ea73f20a577c567049c3cbde56b11393ebad4
remciszewski <remciszewski@icloud.com>
2024-05-19 17:38:45 +0200

    add contact logic (not working yet)

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,41 +1,75 @@
+import { useRef } from 'react'
 import './index.scss'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
-    return (
-        <div>
-            <div className='container contact-page'>
-                <div className='text-zone'>
-                    <h1>
-                        Contact me
-                    </h1>
-                    <p>
-                        acdefgh
-                    </p>
-                    <div className='contact-form'>
-                        <form>
-                            <ul>
-                                <li className='half'>
-                                    <input type='text' name='name' placeholder='Name' required />
-                                </li>
-                                <li className='half'>
-                                    <input type='email' name='email' placeholder='Email' required />
-                                </li>
-                                <li>
-                                    <input placeholder='Subject' type='text' name='subject' required />
-                                </li>
-                                <li>
-                                    <textarea placeholder='Message' name='message' required></textarea>
-                                </li>
-                                <li>
-                                    <input type='submit' className='flat-button' value='SEND' />
-                                </li>
-                            </ul>
-                        </form>
-                    </div>
-                </div>
-            </div>
+
+    const refForm = useRef()
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+            .sendForm(
+                'contact-serivce',
+                'contact-form',
+                refForm.current,
+            )
+            .then(
+                () => {
+                    alert('Message successfully sent!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, please try again!')
+                }
+            )
+    }
+
+  return (
+    <div>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>Contact me</h1>
+          <p>acdefgh</p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Contact
